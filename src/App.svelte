489a0b595e31f2f11509b5159de8dc50c2a0e86c
silloi <silloi1993@gmail.com>
2021-08-22 12:03:40 +0900

    feature: modify design and add properties

--- src/App.svelte
+++ src/App.svelte
@@ -7,31 +7,41 @@
 	
 	let list = [
 		{
+      title: 'Title1 title TITLE',
 			text: lipsum,
 			color: '#85d78b',
 			image: 'https://live.staticflickr.com/4358/36106906420_38e7124a19_b.jpg',
 			emoji: '🧐',
+      qr: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/QR_code_desktop_Japanese_Wikipedia.svg/1200px-QR_code_desktop_Japanese_Wikipedia.svg.png',
 		},
 		{
+      title: 'Title2 title TITLE',
 			text: lipsum,
 			color: '#71d077',
 			emoji: '👶',
+      qr: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/QR_code_desktop_Japanese_Wikipedia.svg/1200px-QR_code_desktop_Japanese_Wikipedia.svg.png',
 		},
 		{
+      title: '',
 			text: lipsum,
 			color: '#5dca64',
 			image: 'https://live.staticflickr.com/65535/51354839188_424b558cd6_b.jpg',
 			emoji: '👑',
+      qr: '',
 		},
 		{
-			text: lipsum,
+      title: 'Title4 title TITLE',
+			text: '',
 			color: '#49c351',
-			emoji: '✨',
+			emoji: '',
+      qr: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/QR_code_desktop_Japanese_Wikipedia.svg/1200px-QR_code_desktop_Japanese_Wikipedia.svg.png',
 		},
 		{
+      title: 'Title5 title TITLE',
 			text: lipsum,
 			color: '#35bd3e',
 			emoji: '🏅',
+      qr: '',
 		},
 	]
 </script>
--- src/Card.svelte
+++ src/Card.svelte
@@ -1,31 +1,47 @@
 <script lang="ts">
+  export let title = ''
   export let text = ''
   export let image = ''
   export let emoji = ''
+  export let qr = ''
 	
-	$: abstract = text.substr(0, 140) + '...'
+	$: abstract = text ? text.substr(0, 140) + '...' : ''
 </script>
 
 <section
-  class="w-screen h-screen px-20 py-40 flex flex-col relative select-none bg-gray-900 text-gray-400"
+  class="w-screen h-screen flex justify-center select-none bg-gray-900 text-gray-300"
 >
 	{#if image}
-	<img alt="" src={image} class="absolute top-0 left-0 w-full object-cover" style="height: 350px;"/>
+  <div class="flex-1">
+    <img alt="" src={image} class="h-full object-cover" />
+  </div>
 	{/if}
-	
-	<div class="relative text-6xl pb-4" style={image ? 'padding-top: 10rem;' : ''}>
-		{emoji}
-	</div>
-	
-	<h2 class="text-4xl">
-			Title title TITLE
-	</h2>
-	
-	<p class="leading-loose py-4">
-		{abstract}
-	</p>		
 
-	<div class="self-end">
-		<img alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/QR_code_desktop_Japanese_Wikipedia.svg/1200px-QR_code_desktop_Japanese_Wikipedia.svg.png" class="w-16 h-16" />
-	</div>
+  <div class="flex-1 flex flex-col justify-center" style="max-width: 976px;">
+    <div class="px-32 flex flex-col">
+      <div class="relative text-6xl pb-4">
+        {emoji}
+      </div>
+      
+      <div>
+        {#if title}
+        <h2 class="text-4xl pb-4">
+          {title}
+        </h2>
+        {/if}
+        
+        {#if text}
+        <p class="leading-loose py-4">
+          {abstract}
+        </p>	
+        {/if}	
+      </div>
+
+      {#if qr}
+      <div class="self-end">
+        <img alt="" src="{qr}" class="w-16 h-16 rounded" />
+      </div>
+      {/if}
+    </div>
+  </div>
 </section>
